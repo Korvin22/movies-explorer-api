4b30fec327069120663618a79d31e29457a9c78e
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-02-08 15:08:14 +0300

    cors

--- app.js
+++ app.js
@@ -31,7 +31,27 @@ const { checkAuth } = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const PORT = 3000;
+
 const app = express();
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'localhost:3001',
+  'https://diplomagud.nomoredomains.rocks/',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  next();
+});
+
 app.use(helmet());
 app.use(limiter);
 /* app.use(express.static(path.join(__dirnamey, 'public'))); */
